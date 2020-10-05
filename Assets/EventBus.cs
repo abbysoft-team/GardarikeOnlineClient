7c8495727776cfb1b861dab43dd50c4cdf5585c5
Artem Popov <Deviling42@gmail.com>
2020-10-05 22:05:37 +0300

    terrain generation

---
+++ Assets/EventBus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBus : MonoBehaviour
+{
+    public const string NEW_BUILDING_EVENT = "NEW_BUILDING";
+    public static EventBus instance;
+
+    //public event Action<BuildItem> onBuildingComplete;
+    public event Action<float[,]> onTerrainGenerationFinished;
+    public event Action<int, int, float[,]> onTerrainLoadingComplete;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //public void buildingComplete(BuildItem building)
+    //{
+    //    onBuildingComplete?.Invoke(building);
+   // }
+
+    public void TerrainGenerationFinished(float[,] heights)
+    {
+        onTerrainGenerationFinished?.Invoke(heights);
+    }
+
+    public void TerrainLoaded(int width, int height, float[,] heights)
+    {
+        onTerrainLoadingComplete?.Invoke(width, height, heights);
+    }
+}

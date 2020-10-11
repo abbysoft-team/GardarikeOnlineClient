57b4973fbece26f170b95d953e8f371feb7ce6fa
HateYourGithub <artemPopovSerg@gmail.com>
2020-10-11 20:16:41 +0300

    sprint 2 fixation

--- Assets/NetworkManagerImpl.cs
+++ Assets/NetworkManagerImpl.cs
@@ -12,16 +12,21 @@ public class NetworkManagerImpl : NetworkManager
     private const string SERVER_URL = "https://server.abbysoft.org:27015";
     private UdpClient client;
     private UdpClient socket;
+    private byte[] buffer;
+    private int multipartLengthBytes;
+    private long multipartLengthPackets;
+
 
     public void ConnectToServer(string login, string password)
     {
         // Listen to server packets 
-        StartListeningThread();
+        StartCommunicationThread();
+
         // Request Map
         GetMapRequest mapRequest = new GetMapRequest
         {
             Location = new Vector3D
-            { 
+            {
                 X = 1,
                 Y = 2,
                 Z = 3
@@ -33,29 +38,65 @@ public class NetworkManagerImpl : NetworkManager
         client.Send(bytes, bytes.Length);
     }
 
-    private void StartListeningThread()
+    private void StartCommunicationThread()
     {
         //start listening for messages and copy the messages back to the client
-        Task.Factory.StartNew(async () => {
+        Task.Factory.StartNew(async () =>
+        {
             while (true)
             {
-                var result = await socket.ReceiveAsync();
-                var response = Response.Parser.ParseFrom(result.Buffer);
+                IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+                var rawResponse = client.Receive(ref endpoint);
 
-                switch (response.DataCase)
+                Debug.Log("received raw response: " + rawResponse.Length);
+
+                if (multipartLengthPackets > 0)
                 {
-                    case Response.DataOneofCase.GetMapResponse:
-                        ProcessMapReply(response.GetMapResponse);
-                        break;
-                    case Response.DataOneofCase.ErrorResponse:
-                        ProcessServerErrorReply(response.ErrorResponse);
-                        break;
-                    default:
-                        ProcessInvalidReply(response);
-                        break;
+                    Debug.Log((multipartLengthPackets - 1) + " to go");
+                    Buffer.BlockCopy(rawResponse, 0, buffer, multipartLengthBytes, rawResponse.Length);
+                    multipartLengthBytes += rawResponse.Length;
+                    multipartLengthPackets--;
+                }
+                else
+                {
+                    Response response;
+                    if (multipartLengthBytes > 0) {
+                        Debug.Log("Received big packet, size: " + multipartLengthBytes);
+
+                        byte[] bigPacket = new byte[multipartLengthBytes];
+                        Buffer.BlockCopy(buffer, 0, bigPacket, 0, multipartLengthBytes);
+                        response = Response.Parser.ParseFrom(bigPacket);
+                    } else {
+                        response = Response.Parser.ParseFrom(rawResponse);
+                    }
+
+                    Debug.Log("Received server packet " + response);
+
+                    switch (response.DataCase)
+                    {
+                        case Response.DataOneofCase.MultipartResponse:
+                            ProcessMultipart(response.MultipartResponse);
+                            break;
+                        case Response.DataOneofCase.GetMapResponse:
+                            ProcessMapReply(response.GetMapResponse);
+                            break;
+                        case Response.DataOneofCase.ErrorResponse:
+                            ProcessServerErrorReply(response.ErrorResponse);
+                            break;
+                        default:
+                            ProcessInvalidReply(response);
+                            break;
+                    }
                 }
             }
         });
+    }
+
+    private void ProcessMultipart(MultipartResponse response)
+    {
+        Debug.Log("Receiving " + response.Parts + " packets as part of one big one...");
+        multipartLengthBytes = 0;
+        multipartLengthPackets = response.Parts;
     }
 
     private void ProcessMapReply(GetMapResponse getMapResponse)
@@ -80,8 +121,11 @@ public class NetworkManagerImpl : NetworkManager
     {
         client = new UdpClient(ipAddress, port);
 
-        IPEndPoint listenAddress = new IPEndPoint(IPAddress.Any, port);
-        socket = new UdpClient(listenAddress);
+        buffer = new byte[1024 * 1024 * 5];
+
+        //listenAddress = new IPEndPoint(IPAddress.Parse(ipAddress), 27015);
+        //socket = new UdpClient(listenAddress);
+        //socket.Client.ReceiveTimeout = 99000;
     }
 
     public void SendEvent(Event eventObject)

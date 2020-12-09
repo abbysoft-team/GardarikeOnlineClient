dafd8f18dc61106fc5e71505f057bd8dfa4e8773
HateYourGithub <artemPopovSerg@gmail.com>
2020-12-09 14:20:05 +0300

    working on capital

---
+++ Assets/Town.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Town : MonoBehaviour
+{
+    public Text population;
+    public Text name;
+    public Text empireName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

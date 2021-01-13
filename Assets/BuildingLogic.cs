23064d24fc2f8593b568babe80460f5e4e33d557
HateYourGithub <artemPopovSerg@gmail.com>
2021-01-13 19:09:53 +0300

    made prototype UI

--- Assets/BuildingLogic.cs
+++ Assets/BuildingLogic.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class BuildingLogic : MonoBehaviour
 {
+    private const float UI_PROTOTYPE_HEIGHT = 40.0f;
     private BuildingState state;
     public Material prototypeMaterial;
     private GameObject building;
+    public GameObject prototypingUI;
 
     void Start()
     {
         EventBus.instance.onChooseLocationForBuilding += ChooseBuildingLocation;
+        prototypingUI.SetActive(false);
     }
+
     public void ChooseBuildingLocation(int eventId, GameObject referenceBuilding)
     {
         Debug.Log("start building");
@@ -22,11 +26,20 @@ public class BuildingLogic : MonoBehaviour
         Utility.SetMaterialForAllChildren(building, prototypeMaterial);
         building.transform.position = Utility.GetPointOnTheGroundInFrontOfCamera();
         building.SetActive(true);
+
+        ConfigurePrototypingUI(building);
+    }
+
+    private void ConfigurePrototypingUI(GameObject prototype)
+    {
+        prototypingUI.transform.position = 
+        new Vector3(prototype.transform.position.x, prototype.transform.position.y + UI_PROTOTYPE_HEIGHT, prototype.transform.position.z);
     }
 
     void Update()
     {
-        var touchHappen = Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Stationary;
+        //var touchHappen = Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Stationary;
+        var touchHappen = Input.GetMouseButton(0);
         if (!touchHappen) return;
 
         if (state == BuildingState.LOCATION_CHOOSE)
@@ -36,6 +49,21 @@ public class BuildingLogic : MonoBehaviour
 
 
         //EventBus.instance.NotifyEventFinished(eventId, building.transform.position);
+    }
+
+    public void ApplyBuilding()
+    {
+
+    }
+
+    public void CancelBuilding()
+    {
+
+    }
+
+    public void EnterRotationMode()
+    {
+
     }
 
     private enum BuildingState {
